486906cea52328038bb6202aa31ff95c73fc0415
Alberto Redondo <2001rs@gmail.com>
2022-12-20 12:24:59 +0100

    commit

--- js/events.js
+++ js/events.js
@@ -1,20 +1,4 @@
 
-// const test = document.getElementsByClassName("section__article");
-
-
-// test.addEventListener("mouseenter", (event) => {
-
-//     // event.target.style.border = "20px solid purple";
-
-// }, false);
-
-// test.addEventListener("mouseover", (event) => {
-
-//     // event.target.style.border = "1px solid orange";
-
-// }, false);
-
-
 peliculas = document.getElementById("mainPeliculas");
 principal = document.getElementById("main");
 peliculas.style.display = "none";
@@ -24,13 +8,10 @@ linkPeliculas = document.getElementById("link__peliculas");
 
 function visibilidad(elem){
     contenedor = document.getElementById(elem);
-    // linkPeliculas.style.fontWeight = "bold";
-    // linkPeliculas.style.font = "bold";
 
     principal.style.display = "none";
     peliculas.style.display = "none";
 
-    // if()
     
     contenedor.style.display = "block";
 }
